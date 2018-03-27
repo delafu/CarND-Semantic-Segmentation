4fd816ec9b06e3d866116952f99cf08cd9a3dd84
delafu <delafu@gmail.com>
2018-03-27 23:57:14 +0200

    commit

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     for i in range(epochs):
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss],
-                        feed_dict={input_image: image, correct_label: label, keep_prob: KEEP_PROB, learning_rate: LEARNING_RATE})
+                        feed_dict={input_image: image, correct_label: label, keep_prob: keep_prob, learning_rate: learning_rate})
             print("Epoch {} Loss: = {:.3f}\n".format(i,loss))
 tests.test_train_nn(train_nn)
 
